530c9242537f636d0d859b2800dfa4714914fda0
Suresh V <126890641+Sureshv24@users.noreply.github.com>
2026-05-14 19:15:59 +0530

    Self-Intro

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SelfIntro from "./SelfIntro";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <SelfIntro />
+    </div>
+  )
+}
---
+++ src/SelfIntro.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function SelfIntro() {
+  return (
+    
+    <div style={{ padding: "20px", border: "1px solid #ccc", margin: "20px" }}>
+      <h2>Self Introduction</h2>
+      <p><strong>Name:</strong> Suresh V</p>
+      <p><strong>Education:</strong> B.Tech / B.E (Information Technology)</p>
+      <p><strong>Skills:</strong> HTML, CSS, JavaScript, React, Python , SQL </p>
+      <p><strong>Location:</strong> Arni,Thiruvannamalai </p>
+      <p><strong>Career Goal:</strong> I Have Bulid the own applications Like zomato,swiggy</p>
+    </div>
+  );
+}
+  
+
