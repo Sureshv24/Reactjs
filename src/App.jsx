a07f662b915e60bdaa6d6b9799c859f17905401f
Suresh V <126890641+Sureshv24@users.noreply.github.com>
2026-05-19 15:19:49 +0530

    Counter

--- src/App.jsx
+++ src/App.jsx
@@ -2,58 +2,39 @@ import React, { useState } from "react";
 import "./App.css";
 
 export default function App() {
- 
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [show, setShow] = useState(true);
 
-  const users = [
-    { id: 1, name: "Lokesh", age: 25, city: "Salem" },
-    { id: 2, name: "Magesh", age: 24, city: "Chennai" },
-    { id: 3, name: "harish", age: 23, city: "Coimbatore" },
-    { id: 4, name: "Dinesh", age: 26, city: "Madurai" },
-    { id: 5, name: "Suresh", age: 22, city: "Trichy" }
-  ];
+  
+  const [count, setCount] = useState(0);
+
+ 
+  const [isDark, setIsDark] = useState(false);
+
+  
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
+
+  const toggleTheme = () => setIsDark(!isDark);
 
   return (
-    <div className="app">
+    <div className={isDark ? "app dark" : "app light"}>
 
-      <h1>React Conditional & List Rendering</h1>
-      
-      <div className="section">
-        <h2>Login Example</h2>
+      <h1>Counter & Theme Toggle</h1>
+      <div className="card">
+        <h2>Counter</h2>
+        <h1 className="count">{count}</h1>
 
-        <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
-          {isLoggedIn ? "Logout" : "Login"}
-        </button>
-
-        <p>
-          {isLoggedIn ? "Welcome back, User!" : "Please login to continue"}
-        </p>
-      </div>
-
-      <div className="section">
-        <h2>Show / Hide Example</h2>
-
-        <button onClick={() => setShow(!show)}>
-          {show ? "Hide" : "Show"}
-        </button>
-
-        {show && <p>This content is visible now </p>}
-      </div>
-
-    
-      <div className="section">
-        <h2>User List</h2>
-
-        <div className="card-container">
-          {users.map((user) => (
-            <div className="card" key={user.id}>
-              <h3>{user.name}</h3>
-              <p>Age: {user.age}</p>
-              <p>City: {user.city}</p>
-            </div>
-          ))}
+        <div className="btn-group">
+          <button onClick={increment}> Increment</button>
+          <button onClick={decrement}> Decrement</button>
+          <button onClick={reset}> Reset</button>
         </div>
+      </div>
+      <div className="card">
+        <h2>Theme</h2>
+        <button onClick={toggleTheme}>
+          {isDark ? " Light Mode" : "Dark Mode"}
+        </button>
       </div>
 
     </div>
