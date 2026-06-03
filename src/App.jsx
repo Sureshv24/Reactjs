16a1a38bceac9f36bc97c505b0090de1978b1772
Suresh V <126890641+Sureshv24@users.noreply.github.com>
2026-06-03 13:53:06 +0530

    custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Users from "./Users";
 import "./App.css";
+import UserTable from "./pages/components/UserTable";
 
 export default function App() {
   return (
     <div>
-      <Users />
+      {/* <Users /> */}
+      <UserTable />
     </div>
   );
 }
---
+++ src/pages/components/UserTable.jsx
@@ -0,0 +1,40 @@
+import useFetchData from "../hooks/useFetchData";
+
+export default function UserTable() {
+  const {data: users,loading,error} = useFetchData(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+  if (loading) {
+    return <h2 className="status">Loading...</h2>;
+  }
+  if (error) {
+    return <h2 className="status">{error}</h2>;
+  }
+  return (
+    <div className="container">
+      <h1>User Information</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Website</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td>{user.phone}</td>
+              <td>{user.website}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
